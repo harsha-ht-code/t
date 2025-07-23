852c0b706348c9d77ac7758d75ab71fc3dabe37b
“harsha-ht-code” <harsha@holithemes.com>
2025-07-23 18:05:08 +0530

    added error file

---
+++ abc.js
@@ -0,0 +1,21 @@
+// vulnerable.js
+
+const express = require('express');
+const { exec } = require('child_process');
+
+const app = express();
+
+app.get('/ping', (req, res) => {
+  const ip = req.query.ip;
+
+  // 🚨 HIGH-RISK: untrusted user input passed directly into shell command
+  exec(`ping -c 1 ${ip}`, (error, stdout, stderr) => {
+    if (error) {
+      res.status(500).send(`Error: ${error.message}`);
+      return;
+    }
+    res.send(`<pre>${stdout}</pre>`);
+  });
+});
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
